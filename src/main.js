5d320ffcc7f138d72d569b377634a99480306bd5
zhangmiao1 <2639840710@qq.com>
2019-02-13 22:29:45 +0800

    简化

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
+import axios from 'axios'
+Vue.prototype.$http=axios;
+axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
