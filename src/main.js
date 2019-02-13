40ae90998bd8c502ef8f0a6415543618e99d33ed
zhangmiao1 <2639840710@qq.com>
2019-02-13 22:55:45 +0800

    登录页面初步

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
+import '@/assets/css/base.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
