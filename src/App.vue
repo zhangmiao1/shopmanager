eaef26b83b99786edd153dc9610ba7508915eed9
zhangmiao1 <2639840710@qq.com>
2019-02-14 17:20:10 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 <<<<<<< HEAD
-    
+
 =======
 
 >>>>>>> dev-login
--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 
-=======
 import login from '@/components/login.vue'
-<<<<<<< HEAD
->>>>>>> dev-login
-=======
+
 import Home from '@/components/home.vue'
->>>>>>> dev-login
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-<<<<<<< HEAD
+
       path: '/',
       name: 'HelloWorld',
       
-=======
+
       path: '/login',
       name: 'login',
       component: login
-<<<<<<< HEAD
->>>>>>> dev-login
-=======
+
     },
     {
       path: '/home',
       name: 'home',
       component: Home
->>>>>>> dev-login
+
     }
   ]
 })
