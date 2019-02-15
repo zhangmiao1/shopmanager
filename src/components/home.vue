85074803e758cdc6de04ca6b0e4ab91210178d51
zhangmiao1 <2639840710@qq.com>
2019-02-15 20:01:46 +0800

    user导航

--- src/components/home.vue
+++ src/components/home.vue
@@ -16,16 +16,69 @@
       </el-header>
       <el-container>
         <el-aside width="200px" class="aside">
-          <homeSilde/>
+          <el-menu default-active="2" class="el-menu-vertical-demo" unique-opened>
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item index="users">
+                <i class="el-icon-menu"></i>
+                用户列表
+              </el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                选项1
+              </el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                选项1
+              </el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                选项1
+              </el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                选项1
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
         </el-aside>
-        <el-main class="main">Main</el-main>
+        <el-main class="main">
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
-import homeSilde from '@/components/home-slide.vue'
+
 export default {
   data () {
     return {}
@@ -36,10 +89,8 @@ export default {
         name: 'login'
       })
     }
-  },
-  components: {
-    homeSilde
   }
+
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,20 @@ import Router from 'vue-router'
 
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
-
+import users from '@/components/users.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      name: 'users',
+      path: '/users',
+      component: users
+    }]
+
   }, {
     name: 'login',
     path: '/login',
