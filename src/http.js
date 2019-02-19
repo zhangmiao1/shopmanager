12e6aa8814b22270b4f246e43c9cf590b5306838
zhangmiao1 <2639840710@qq.com>
2019-02-19 11:02:40 +0800

    '调整'

--- src/http.js
+++ src/http.js
@@ -4,7 +4,7 @@ MyPlugin.install = function (Vue) {
     axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
     axios.interceptors.request.use(function (config) {
         // 在发起请求请做一些业务处理
-        console.log(config)
+       
         if (config.url !== 'login') {
             const AUTH_TOKEN = localStorage.getItem("token");
             config.headers["Authorization"] = AUTH_TOKEN;
