4c241b1c88e1c8ee6dfa4a2170ad31f13ad30999
zhangmiao1 <2639840710@qq.com>
2019-02-21 11:19:28 +0800

    '查询-编辑'

--- src/components/goodslist.vue
+++ src/components/goodslist.vue
@@ -5,7 +5,13 @@
     <!-- 搜索框 -->
     <el-row class="searchArea">
       <el-col :span="24">
-        <el-input v-model="searchValue" class="searchInput" clearable placeholder="请输入内容">
+        <el-input
+          v-model="query"
+          class="searchInput"
+          clearable
+          placeholder="请输入内容"
+          @clear="clearQuery()"
+        >
           <el-button @click="handleSearch" slot="append" icon="el-icon-search"></el-button>
         </el-input>
         <el-button @click="$router.push({name:'goodsadd'})" type="success" plain>添加商品</el-button>
@@ -29,7 +35,14 @@
 
       <el-table-column label="操作" width="100">
         <template slot-scope="scope">
-          <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button
+            plain
+            size="mini"
+            type="primary"
+            icon="el-icon-edit"
+            circle
+            @click="editgoods(scope.row)"
+          ></el-button>
           <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
         </template>
       </el-table-column>
@@ -45,6 +58,28 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+    <!-- 对话框 --编辑商品 -->
+    <el-dialog title="编辑商品" :visible.sync="dialogFormVisibleEdit">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="商品名称">
+          <el-input v-model="formdata.goods_name" disabled></el-input>
+        </el-form-item>
+
+        <el-form-item label="商品数量">
+          <el-input v-model="formdata.goods_number"></el-input>
+        </el-form-item>
+        <el-form-item label="商品价格">
+          <el-input v-model="formdata.goods_price"></el-input>
+        </el-form-item>
+        <el-form-item label="商品重量">
+          <el-input v-model="formdata.goods_weight"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+        <el-button type="primary" @click="editGoodsData()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -52,17 +87,73 @@
 export default {
   data() {
     return {
-      searchValue: "",
+      query: "",
       list: [],
       pagenum: 1,
       pagesize: 10,
-      total: 0
+      total: 0,
+      //编辑商品
+      dialogFormVisibleEdit: false,
+
+      formdata: {
+        goods_name: "",
+        goods_number: "",
+        goods_price: "",
+
+        goods_weight: ""
+      }
     };
   },
-  created() {
-    this.loadData();
-  },
   methods: {
+    async editgoods(goods) {
+      this.dialogFormVisibleEdit = true;
+
+      const res = await this.$http.get(`goods/${goods.goods_id}`);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.formdata = data;
+      }
+    },
+    //编辑商品
+    async editGoodsData() {
+      const res = await this.$http.put(
+        `goods/${this.formdata.goods_id}`,
+        this.formdata
+      );
+      console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.$message.success(msg);
+        this.dialogFormVisibleEdit = false;
+        this.loadData();
+      }
+    },
+    //清空
+    clearQuery() {
+      this.loadData();
+    },
+    //查询
+    handleSearch() {
+      this.pagenum = 1;
+      this.loadData();
+    },
+    async loadData() {
+      const { data: resData } = await this.$http.get(
+        `goods?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      );
+      this.total = resData.data.total;
+      this.list = resData.data.goods;
+
+      // console.log(this.list);
+    },
     handleSizeChange(val) {
       // console.log(`每页 ${val} 条`)
       this.pagesize = val;
@@ -73,16 +164,10 @@ export default {
       // console.log(`当前页: ${val}`)
       this.pagenum = val;
       this.loadData();
-    },
-    handleSearch() {},
-    async loadData() {
-      const { data: resData } = await this.$http.get(
-        `goods?pagenum=${this.pagenum}&pagesize=${this.pagesize}`
-      );
-      this.total = resData.data.total;
-      this.list = resData.data.goods;
-      // console.log(this.list)
     }
+  },
+  created() {
+    this.loadData();
   }
 };
 </script>
